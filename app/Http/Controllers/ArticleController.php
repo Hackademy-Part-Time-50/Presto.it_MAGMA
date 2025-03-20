fc1025ad06847648b1890e094c705287e9485c80
Ale-Marche00 <marchetti.canossa@gmail.com>
2025-03-20 19:35:22 +0100

    added routes for cards

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -22,7 +22,7 @@ class ArticleController extends Controller
 
     public function show(Article $article)
     {
-        return view('article.show', compact('article'));
+        return view('articles.show', compact('article'));
     }
 
 
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,11 +1,4 @@
 <x-layouts.layout>
-
-
-
-
-
-
-
     <div class="container-fluid p-4 d-flex justify-content-center prodotto">
         <div class="row w-100 justify-content-center align-items-center">
             {{-- carosello --}}
@@ -29,7 +22,6 @@
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Indietro</span>
-                </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Avanti</span>
