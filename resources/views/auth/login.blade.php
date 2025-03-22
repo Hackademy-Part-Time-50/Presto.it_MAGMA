4c51bfe5220f03ba946fd9430a1b36879555bbd6
Marietto05 <mario.odoviciuc@gmail.com>
2025-03-22 12:30:58 +0100

    Modifica form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,22 +8,7 @@
 
         <div class="row justify-content-center align-items-center">
             <div class="col-12 col-md-6">
-                <form method="POST" action="{{ route('login') }}" class="bg-body-tertiary shadow rounded p-5">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="loginEmail" class="form-label">Indirizzo Email</label>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="loginEmail" name="email">
-                        @error('email') <span class="small fst-italic text-danger">{{ $message }}</span> @enderror 
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-                        @error('password') <span class="small fst-italic text-danger">{{ $message }}</span> @enderror
-                    </div>
-                    <div class="d-flex justify-content-center">
-                        <button type="submit" class="btn btn-dark">Accedi</button>
-                    </div>
-                </form>
+                <x-form />
             </div>
             <div class="d-flex flex-column align-items-center mt-4">
                 <h6>Oppure</h6>
