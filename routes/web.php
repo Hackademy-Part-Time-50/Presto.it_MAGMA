d48e60e7de2c6b4bce8aa27c9b314da03227c0bf
Alexax97 <alessioxale@gmail.com>
2025-03-23 14:42:02 +0100

    creato comando automazione revisore,creata dashboard da migliorare es:aggiungere pulsante navbar per raggiungere la dashboard in modo comodo

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use App\Http\Middleware\RedirectIfNotAuthenticated;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\RevisorController;
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
 
@@ -13,6 +14,6 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/articles/index',[ArticleController::class, 'index'])->name('articles.index');
 Route::get('/show/article/{article}',[ArticleController::class, 'show'])->name('articles.show');
 Route::get('/category/{category}', [ArticleController::class, 'byCategory'])->name('byCategory');
-
+Route::get('/revisor/index',[RevisorController::class, 'index'])->name('revisor.index');
 
 
