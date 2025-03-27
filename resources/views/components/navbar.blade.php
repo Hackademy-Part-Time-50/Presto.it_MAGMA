425282026e771b274dd174ac5eeb6af9d8c5314a
Ale-Marche00 <marchetti.canossa@gmail.com>
2025-03-27 13:13:34 +0100

    main rebased

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,8 +2,6 @@
 
   <div class="container-fluid">
 
-
-
     <a class="navbar-brand text-white fw-bold fs-4" href="{{ route('homepage') }}"><img
         src="{{ Storage::url('image/icons.svg') }}" alt="Presto"></a>
 
@@ -11,45 +9,19 @@
       data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
       aria-label="Toggle navigation">
 
-
       <span class="navbar-toggler-icon custom-toggler"></span>
-
-
 
     </button>
 
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
 
         <li class="nav-item">
           <a class="nav-link text-white" href="{{ route('articles.index') }}">{{__('ui.allannounces')}}</a>
         </li>
 
-
-
-
-
-
-
         <li class="nav-item dropdown">
-<<<<<<< HEAD
-
-          <a href="#" class="nav-link dropdown-toggle text-white" role="button" data-bs-toggle="dropdown"
-            aria-expanded="false">Categorie</a>
-
-          <ul class="dropdown-menu futuristic-menu">
-            @foreach ($categories as $category)
-        <li><a href="{{ route('byCategory', ['category' => $category]) }}"
-          class="dropdown-item text-capitalize">{{ $category->name }}</a></li>
-        @if (!$loop->last)
-      <hr class="dropdown-divider category-divider">
-    @endif
-      @endforeach
-=======
           <a href="#" class="nav-link dropdown-toggle text-white" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{__('ui.nav_category')}}</a>
           <ul class="dropdown-menu futuristic-menu">
             @foreach ($categories as $category)
@@ -58,13 +30,7 @@
                 <hr class="dropdown-divider category-divider">
               @endif
             @endforeach
->>>>>>> 4e9d88c (ending translation)
           </ul>
-
-
-
-
-          
         </li>
       </ul>
 
@@ -73,63 +39,19 @@
         {{-- ricerca --}}
         <div class=" ">
 
-
           <form action="{{ route('article.search') }}" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search"
             method="GET">
-
-
             @if (session()->has('error_search'))
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <input type="search" name="query" class="form-control is-invalid" placeholder="Non può essere vuoto"
-          aria-label="Search">
-      @else
-    <input type="search" name="query" class="form-control" placeholder="Cerca" aria-label="Search">
-  @endif
 
-
-
+             <input type="search" name="query" class="form-control is-invalid" placeholder="Non può essere vuoto"
+              aria-label="Search">
+            @else
+              <input type="search" name="query" class="form-control" placeholder="Cerca" aria-label="Search">
+            @endif
           </form>
-
 
         </div>
         {{-- fine ricerca --}}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         @auth
       <li class="nav-item dropdown">
@@ -163,50 +85,6 @@
     </li>
 
   @endauth
-=======
-=======
->>>>>>> 65de757 (finishing translate)
-              <input type="search" name="query" class="form-control is-invalid" placeholder="can't be empty" aria-label="search">
-            @else
-              <input type="search" name="query" class="form-control" placeholder="{{__('ui.nav_search')}}" aria-label="search">
-            @endif
-              <button type="submit" class="input-group-text btn btn_custom" id="basic-addon2">
-              {{__('ui.nav_search')}}
-              </button>
-          </div>
-        </form>
-        @auth
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              {{ Auth::user()->name }}
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end futuristic-menu">
-              <li><a class="dropdown-item" href="{{ route('create.article') }}">{{__('ui.create_announce')}}</a></li>
-              @if(Auth::user()->is_revisor)
-                <li class="nav-item"> 
-                  <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25"
-                    href="{{ route('revisor.index') }}">{{__('ui.revisor_zone')}}
-                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    {{ \App\Models\Article::toBeRevisedCount() }}
-                  </span>
-                  </a>
-                </li>
-              @endif
-              <li>
-                <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">{{__('ui.logout')}}</a>
-              </li>
-              <form action="{{ route('logout') }}" method="POST" class="d-none" id="form-logout">@csrf</form>
-            </ul>
-          </li>
-        @else
-          <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('login') }}">{{__('ui.login')}}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white" href="{{ route('register') }}">{{__('ui.signin')}}</a>
-          </li>
-        @endauth
->>>>>>> 4e9d88c (ending translation)
       </ul>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -39,25 +39,3 @@ Route::get('/show/article/{article}',[ArticleController::class, 'show'])->name('
 Route::get('/category/{category}', [ArticleController::class, 'byCategory'])->name('byCategory');
 Route::get('/search/article', [ArticleController::class, 'searchArticle'])->name('article.search');
 
-<<<<<<< HEAD
-//revisore
-Route::get('/revisor/index',[RevisorController::class, 'index'])->middleware('isRevisor')->name('revisor.index');
-
-//logica di accettazione articolo
-Route::patch('/accept/{article}', [RevisorController::class, 'accept'])->name('accept');
-
-//logica di rifiuto articolo
-Route::patch('/reject/{article}', [RevisorController::class, 'reject'])->name('reject');
-
-//annullare accettazione o rifiuto articolo
-Route::post('/cancel', [RevisorController::class, 'cancelLastAction'])->middleware('isRevisor')->name('cancel.lastAction');
-
-//rotta per cambiare la lingua
-Route::post('/lingua/{lang}', [PublicController::class, 'setLanguage'])->name('setLocale');
-
-Route::get('revisor/request',[RevisorController::class, 'becomeRevisor'])->middleware('auth')->name('become.revisor');
-
-Route::get('/make/revisor/{user}', [RevisorController::class, 'makeRevisor'])->name('make.revisor');
-
-=======
->>>>>>> 569ed01 (creted files for languages)
