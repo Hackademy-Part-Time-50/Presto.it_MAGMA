ff73d46b7028942c10f6d343c1058eb7659c132b
Ale-Marche00 <marchetti.canossa@gmail.com>
2025-04-03 19:45:08 +0200

    created job for valutation images

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -44,6 +44,72 @@
                                         <img src="{{ $image->getUrl(200, 200) }}" class="img-fluid rounded shadow" 
                                         alt="Immagine {{ $key +1 }} dell'articolo {{$article_to_check->title}}">
                                     </div>
+
+                                    <div class="col-md-8 ps-3">
+                                        <div class="card-body">
+                                            <h5 class="">Ratings</h5>
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->adult }}">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-10">
+                                                    Adult
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->violence }}">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-10">
+                                                    Violence
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->spoof }}">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-10">
+                                                    Spoof
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->racy }}">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-10">
+                                                    Racy
+                                                </div>
+
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->medical }}">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-10">
+                                                    Medical
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
                                 @endforeach
                             @else
                                 @for ($i = 0; $i < 6; $i++)
